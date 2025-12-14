0f0e2c7e1803a18fa595b87a9e7cdb6ac3ebe31e
dmakesapps <davisapoore@gmail.com>
2025-12-13 16:35:34 -0800

    Cleanup: Remove unused code, add widget target back permanently

--- Muse/PromiseKeeperApp.swift
+++ Muse/PromiseKeeperApp.swift
@@ -1,14 +1,8 @@
 import SwiftUI
 import SwiftData
-import UserNotifications
 
 @main
 struct MuseApp: App {
-    init() {
-        NotificationManager.requestPermission()
-        setupNotificationDelegate()
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -17,7 +11,7 @@ struct MuseApp: App {
     }
     
     private func createModelContainer() -> ModelContainer {
-        let schema = Schema([Promise.self, Message.self, UserProfile.self, NotificationAgent.self, Affirmation.self])
+        let schema = Schema([AffirmationSession.self])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
         
         do {
@@ -26,54 +20,4 @@ struct MuseApp: App {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }
-    
-    private func setupNotificationDelegate() {
-        UNUserNotificationCenter.current().delegate = NotificationDelegate.shared
-    }
 }
-
-// MARK: - Notification Delegate
-class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
-    static let shared = NotificationDelegate()
-    
-    // Handle notification when app is in foreground
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-    ) {
-        completionHandler([.banner, .sound])
-    }
-    
-    // Handle notification actions
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void
-    ) {
-        let promiseIdString = response.notification.request.content.userInfo["promiseId"] as? String
-        guard let promiseIdString = promiseIdString,
-              let promiseId = UUID(uuidString: promiseIdString) else {
-            completionHandler()
-            return
-        }
-        
-        // Handle "I Kept It" action
-        if response.actionIdentifier == "KEPT_ACTION" {
-            // Post notification to app to handle the completion
-            // The notification time can be inferred from when the notification was scheduled
-            // We'll use the current time as the completion time
-            NotificationCenter.default.post(
-                name: NSNotification.Name("PromiseKeptFromNotification"),
-                object: nil,
-                userInfo: [
-                    "promiseId": promiseId,
-                    "completionTime": Date()
-                ]
-            )
-        }
-        
-        completionHandler()
-    }
-}
-
